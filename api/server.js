64f8df8b105317ccf4be6a2acb9ba4e65da3beaa
e1806inat <j846002b@mails.cc.ehime-u.ac.jp>
2022-10-05 13:29:01 +0900

    初期設定

--- api/server.js
+++ api/server.js
@@ -1,1 +1,13 @@
-console.log("nodejs");
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello Express");
+});
+
+app.listen(PORT, () => {
+    console.log("サーバー起動中・・・");
+});

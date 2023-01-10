0830bb055e0233fcf3af45943749c1a6c61d65f1
e1766mori <j846029b@mails.cc.ehime-u.ac.jp>
2023-01-10 16:19:10 +0900

    0110

--- frontend/src/AllPage/InputAll/Sokuho/SokuhoMakegamePage/EditMakeGamePopup/EditMakegamePopup.jsx
+++ frontend/src/AllPage/InputAll/Sokuho/SokuhoMakegamePage/EditMakeGamePopup/EditMakegamePopup.jsx
@@ -24,17 +24,27 @@ class Popup extends React.Component {
                         }}
                     ></input>大会を編集する<br></br>
 
+                    変更前：{this.props.gameInfo.game_ymd}<br />
+                    変更後：
+                    年{this.props.makePulldown(0, this.props.YearList, "year", this.props.editingSelectedYmd, this.props.setEditingSelectedYmd)}
+                    月{this.props.makePulldown(1, this.props.MonthList, "month", this.props.editingSelectedYmd, this.props.setEditingSelectedYmd)}
+                    日{this.props.makePulldown(2, this.props.DayList, "day", this.props.editingSelectedYmd, this.props.setEditingSelectedYmd)}<br />
+
                     回戦の変更<br />
-                    変更前：{this.props.member.grade}<br />
-                    変更後：{this.props.makePulldown(0, this.props.gradeArray, "grade", this.props.editGrade, this.props.setEditGrade)}<br />
+                    変更前：{this.props.gameInfo.match_num}<br />
+                    変更後：{this.props.makePulldown(0, this.props.iningList, "ining", this.props.editingSelected, this.props.setEditingSelected)}<br />
 
-                    先攻の高校を変更
-                    変更前：
-                    変更後：
+                    先攻の高校を変更<br />
+                    変更前：{this.props.gameInfo.school_name}<br />
+                    変更後：{this.props.makePulldown(1, this.props.Schools, "school_name", this.props.editingSelected, this.props.setEditingSelected)}<br />
 
-                    後攻の高校を変更
-                    変更前：
-                    変更後：
+                    後攻の高校を変更<br />
+                    変更前：{this.props.gameInfo.school_name_2}<br />
+                    変更後：{this.props.makePulldown(2, this.props.Schools, "school_name", this.props.editingSelected, this.props.setEditingSelected)}<br />
+
+                    会場を変更<br />
+                    変更前：{this.props.gameInfo.venue_name}<br />
+                    変更後：{this.props.makePulldown(3, this.props.Venues, "venue_name", this.props.editingSelected, this.props.setEditingSelected)}<br />
 
 
                     {/* 削除チェックボックス */}
@@ -57,12 +67,44 @@ class Popup extends React.Component {
                     <nbsp></nbsp>
 
                     {/* はいのボタン */}
-
                     {(true) &&
-
                         <button className="button_style"
                             onClick={() => {
                                 this.props.closePopup()
+
+                                if (this.props.EorDCheckbox) {
+
+                                    let sendInfo = {
+                                        game_id: this.props.gameInfo.game_id,
+                                        tournament_id: this.props.gameInfo.tournament_id,
+                                        school_id_1: this.props.Schools[this.props.editingSelected[1]].school_id,
+                                        school_id_2: this.props.Schools[this.props.editingSelected[2]].school_id,
+                                        venue_id: this.props.Venues[this.props.editingSelected[3]].venue_id,
+                                        match_num: this.props.iningList[this.props.editingSelected[0]].ining,
+                                        first_rear_1: this.props.gameInfo.first_rear_1,
+                                        first_rear_2: this.props.gameInfo.first_rear_2,
+                                        game_ymd: this.props.YearList[this.props.editingSelectedYmd[0]].year + "-" +
+                                            this.props.MonthList[this.props.editingSelectedYmd[1]].month + "-" +
+                                            this.props.DayList[this.props.editingSelectedYmd[2]].day,
+                                        match_results: this.props.gameInfo.match_results
+                                    }
+
+                                    this.props.EditGame(sendInfo)
+
+                                }
+
+                                else {
+                                    let sendInfo = {
+                                        game_id: this.props.gameInfo.game_id,
+                                        tournament_id: this.props.gameInfo.tournament_id
+                                    }
+
+                                    this.props.DeleteGame(sendInfo)
+
+                                }
+
+                                this.props.loadGame(this.props.setGameInfoState, this.props.urlTournamentId)
+
                             }}>はい
                         </button>
 
@@ -108,30 +150,46 @@ class EditMakegamePopup extends React.Component {
                         this.togglePopup()
 
                     }}
-                    className={"InputMember" + this.props.selectedMember[this.props.ind]}
                 >
-                    <div className="selectName">
-                        <div> &nbsp;&nbsp;{this.props.member.grade}年</div>
-                        <div className="playerName">&nbsp;&nbsp;&nbsp;&nbsp;
-                            {this.props.member.player_name_kanji}（ {this.props.member.player_name_hira}）</div>
-                    </div>
-                    {/* &nbsp; 背番号{member.uniform_number}  */}
-                    <div className="Dominant">&nbsp;
-                        {this.props.member.handed_hit}打 &nbsp;
-                        {this.props.member.handed_throw}投 &nbsp;
-                        背番号:{this.props.uniformNumberArray[this.props.ind]}
+
+                    <div className="game">
+                        <button className="btn_So_Make"
+                            onClick={() => { }}>
+                            {this.props.gameInfo.match_num}回戦<br />
+                            {this.props.gameInfo.school_name}<br />
+                            {this.props.gameInfo.school_name_2}<br />
+                            {
+                                this.props.Venues.length !== 0 &&
+                                this.props.Venues[this.props.gameInfo.venue_id].venue_name
+                            }
+                        </button><br /><br />
                     </div>
 
                 </button>
+
                 {/* this.state.PopupがtrueであればPopupウィンドウを開く */}
                 {this.state.showPopup ? (
                     <Popup text="確認画面" closePopup={this.togglePopup}
-                        game={this.props.game}
+                        gameInfo={this.props.gameInfo}
                         ind={this.props.ind}
                         EorDCheckbox={this.props.EorDCheckbox}
                         setEorDCheckbox={this.props.setEorDCheckbox}
                         makePulldown={this.props.makePulldown}
-                        isEnpty={this.props.isEnpty}
+                        iningList={this.props.iningList}
+                        Schools={this.props.Schools}
+                        Venues={this.props.Venues}
+                        YearList={this.props.YearList}
+                        MonthList={this.props.MonthList}
+                        DayList={this.props.DayList}
+                        editingSelected={this.props.editingSelected}
+                        setEditingSelected={this.props.setEditingSelected}
+                        editingSelectedYmd={this.props.editingSelectedYmd}
+                        setEditingSelectedYmd={this.props.setEditingSelectedYmd}
+                        EditGame={this.props.EditGame}
+                        DeleteGame={this.props.DeleteGame}
+                        loadGame = {this.props.loadGame}
+                        setGameInfoState={this.props.setGameInfoState}
+                        urlTournamentId={this.props.urlTournamentId}
                     />
                 ) : null}
             </div>

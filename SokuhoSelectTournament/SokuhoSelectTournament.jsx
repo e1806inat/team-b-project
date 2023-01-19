2b3ef590def2e1c6d6a44e02c09e43f70262fe0a
h077otsu <108774348+h077otsu@users.noreply.github.com>
2023-01-20 05:03:31 +0900

    Add files via upload

---
+++ SokuhoSelectTournament/SokuhoSelectTournament.jsx
@@ -0,0 +1,166 @@
+import React, { useRef, useState, useEffect } from "react";
+import { Link, useNavigate } from 'react-router-dom'
+import { Option } from '../../../OtherPage/Option'
+import './SokuhoSelectTournament.css'
+import MediaQuery from "react-responsive";
+
+const readTournament = (setTournamentData) => {
+  fetch("http://localhost:5000/tournament/tournament_call", {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => setTournamentInfo(setTournamentData, data))
+}
+
+const setTournamentInfo = (setTournamentData, data) => {
+  console.log(data)
+  setTournamentData(data)
+}
+
+//文字分割
+const dateSplit = (nowdate) => {
+  let dateArray = nowdate.split('-');
+  dateArray = { "year": dateArray[0], "month": dateArray[1], "day": dateArray[2] }
+  return dateArray
+}
+
+
+export const Input_Tournament = () => {
+  const birthYearRef = useRef(null);
+  const birthMonthRef = useRef(null);
+
+  const InitialYear = 2020
+  const InitialMonth = 1
+
+  const [birthYear, setBirthYear] = useState(InitialYear);
+  const [birthMonth, setBirthMonth] = useState(InitialMonth);
+  //文字分割のための箱を用意
+  let dateArray = { "year": "", "month": "", "day": "" }
+
+  let [TournamentData, setTournamentData] = useState([
+    { "tournament_name": '第31回春大会', "tournament_id": '55', "opening": "2022-03-01" },
+    { "tournament_name": '第9回松山大会', "tournament_id": '56', "opening": "2022-02-01" },
+    { "tournament_name": '第5回伊予大会', "tournament_id": '57', "opening": "2022-01-01" },
+    { "tournament_name": '第22回秋大会', "tournament_id": '58', "opening": "2021-08-01" }
+  ])
+
+  //ページ遷移用
+  const navigate = useNavigate()
+  const PageTransition = (url) => {
+    navigate(url)
+  }
+
+  const setYear = () => {
+    for (let i = InitialYear; i <= new Date().getFullYear() + 10; i++) {
+      const option = document.createElement('option');
+      option.value = i;
+      option.text = i;
+      birthYearRef.current.appendChild(option);
+    }
+  }
+
+  const setMonth = () => {
+    for (let i = InitialMonth; i <= 12; i++) {
+      const option = document.createElement('option');
+      option.value = i;
+      option.text = i;
+      birthMonthRef.current.appendChild(option);
+    }
+  }
+
+
+
+  const selectBirthYear = (e) => {
+    setBirthYear(e.target.value);
+  }
+
+  const selectBirthMonth = (e) => {
+    setBirthMonth(e.target.value);
+  }
+
+
+
+  useEffect(() => {
+    setYear();
+    setMonth();
+    readTournament(setTournamentData);
+  }, []);
+
+  return (
+    <div>
+      <Option></Option>
+      <p></p>
+      <header>
+                <div class="heading">大会選択画面</div>
+                <div class="back">
+                  <button>
+                      {/* <Link to> */}
+                      ＜戻る
+                      {/* </Link> */}
+                  </button>
+                </div>
+            </header>
+      <br />
+      
+      <div class="selectdiv_So_SoTo">
+
+        <label>
+          <select ref={birthYearRef} value={birthYear} onChange={selectBirthYear}></select>
+        </label>
+      </div>
+      <div class="moji">
+          年
+      </div>
+      <div class="selectdiv_So_SoTo">
+        <label>
+          <select ref={birthMonthRef} value={birthMonth} onChange={selectBirthMonth}></select>
+        </label>
+      </div>
+      <div class="moji">
+          月
+      </div>
+      <div className="tournamentList">
+        <br />
+        <br />
+        <br />
+        <div className="tournaments">
+
+          {TournamentData.map((Tournament, ind) => {
+            //文字分割
+            dateArray = dateSplit(Tournament.opening)
+
+            return (
+              <div class="whole_Sokuho">
+
+              <div className="tournament">
+                <div className="days">
+                  <span>{dateArray.year}年{dateArray.month}月{dateArray.day}日</span>
+                </div>
+                <div className="tournamentName">
+                  <button class="btn_So_Tour"
+                    onClick={() =>
+                      PageTransition(
+                        "sokuho_input_makegame?urlTournamentId=" +
+                        Tournament.tournament_id +
+                        "&urlTournamentName=" +
+                        Tournament.tournament_name
+                      )
+                    }>
+                    {Tournament.tournament_name}
+                  </button>
+                </div>
+              </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Input_Tournament;

7c5caff11780077c69bafdf0d9f0e1813a3d5c85
h076otan <108774103+h076otan@users.noreply.github.com>
2023-01-20 10:30:21 +0900

    過去データ参照

---
+++ RefTournamentSchool/TournamentMember.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import "./RefTournamentMember.css"
+
+const TournamentMember = ({member}) => {
+
+  return (
+    <table border="1" className='members_t'>
+        <tr>
+        <td width="5%" rowspan="2">{member.grade}</td>
+            <td width="30%">{member.player_name_hira}</td>
+            <td width="5%" rowspan="2">{member.handed_hit}</td>
+            <td width="5%" rowspan="2">{member.handed_throw}</td>
+            <td width="8%" rowspan="2">{member.hit_num}</td>
+            <td width="8%" rowspan="2">{member.bat_num}</td>
+            <td width="8%" rowspan="2">{member.BA}</td>
+        </tr>
+        <tr>
+            <td width="30%" rowspan="2">{member.player_name_kanji}</td>
+        </tr>
+    </table>
+  )
+}
+
+export default TournamentMember;

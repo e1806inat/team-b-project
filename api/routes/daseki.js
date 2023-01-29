164d3fd0b5129d732a0f4ec2128d9f15f4380db2
e1806inat <j846002b@mails.cc.ehime-u.ac.jp>
2023-01-29 11:59:17 +0900

    1/29

--- api/routes/daseki.js
+++ api/routes/daseki.js
@@ -16,12 +16,12 @@ router.get("/", (req, res) => {
 //テスト用
 router.post("/daseki_register_for_test", async (req, res, next) => {
     //const { table_name, inning, game_id, school_id, player_id, pitcher_id, score, total_score, batting_order, outcount, base, text_inf, pass, touched_coordinate, ball_kind, hit, foreball, deadball, pinch } = req.body;
-    const tmp_table_name = `test_pbl.` + `test116`;
+    const tmp_table_name = `test_pbl.` + `test125`;
 
     try {
         //打席情報登録
-        for(var values of req.body){
-            await executeQuery(`insert into ${tmp_table_name} values ("0", ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [values.inning, values.game_id, values.school_id, values.player_id, values.pitcher_id, values.score, values.total_score, values.outcount, values.base, values.text_inf, values.pass, values.touched_coordinate, values.ball_kind, values.hit, values.foreball, values.deadball, values.pinch,  values.batting_order]);
+        for (var values of req.body) {
+            await executeQuery(`insert into ${tmp_table_name} values ("0", ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [values.inning, values.game_id, values.school_id, values.player_id, values.pitcher_id, values.score, values.total_score, values.outcount, values.base, values.text_inf, values.pass, values.touched_coordinate, values.ball_kind, values.hit, values.foreball, values.deadball, values.pinch, values.batting_order]);
         }
         //await executeQuery(`insert into ${tmp_table_name} values ("0", ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [inning, game_id, school_id, player_id, pitcher_id, score, total_score, outcount, base, text_inf, pass, touched_coordinate, ball_kind, hit, foreball, deadball, pinch,  batting_order]);
         res.end("OK");
@@ -40,7 +40,7 @@ router.post("/daseki_register", async (req, res, next) => {
 
     try {
         //打席情報登録
-        await executeQuery(`insert into ${tmp_table_name} values ("0", ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [inning, game_id, school_id, player_id, pitcher_id, score, total_score, outcount, base, text_inf, pass, touched_coordinate, ball_kind, hit, foreball, deadball, pinch,  batting_order]);
+        await executeQuery(`insert into ${tmp_table_name} values ("0", ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [inning, game_id, school_id, player_id, pitcher_id, score, total_score, outcount, base, text_inf, pass, touched_coordinate, ball_kind, hit, foreball, deadball, pinch, batting_order]);
         res.end("OK");
     }
     catch (err) {
@@ -61,7 +61,7 @@ router.post("/tmp_table_create", async (req, res, next) => {
         //一時打席情報登録用テーブル作成
         await executeQuery(`create table ${table_name} (at_bat_id int not null auto_increment, inning int, game_id int not null,  school_id int not null, player_id int not null, pitcher_id int not null, score int, total_score int, outcount int, base char(3), text_inf varchar(300), pass bool, touched_coordinate varchar(100), ball_kind varchar(10), hit bool, foreball bool, deadball bool, pinch varchar(300), batting_order int, primary key(at_bat_id, game_id), foreign key(school_id) references t_school(school_id),
         foreign key(player_id) references t_player(player_id))`);
-        res.end('OK');   
+        res.end('OK');
     }
     catch (err) {
         //next(err);
@@ -69,7 +69,7 @@ router.post("/tmp_table_create", async (req, res, next) => {
         console.log(err)
         next(err);
     }
-});  
+});
 
 //一時打席情報登録用のテーブル削除（運用者用webアプリ）
 router.post("/tmp_table_delete", async (req, res, next) => {
@@ -83,7 +83,7 @@ router.post("/tmp_table_delete", async (req, res, next) => {
         //一時テーブルの情報が蓄積テーブルに登録されているか確認
         const count_of_bat = await tran.query('select count(*) from t_at_bat where game_id = ?', [game_id]);
         const count_of_tmp = await tran.query(`select count(*) from ${tmp_table_name}`);
-        
+
         if (count_of_bat[0]['count(*)'] >= 1 && count_of_bat[0]['count(*)'] == count_of_tmp[0]['count(*)']) {
             await tran.query(`drop table ${tmp_table_name}`);
             await tran.commit();
@@ -163,7 +163,7 @@ router.post("/tmp_daseki_transmission", async (req, res, next) => {
 
         return res.json(rows);
     }
-    catch (err) { 
+    catch (err) {
         console.log(err);
         //await tran.rollback();
         next(err);
@@ -172,7 +172,7 @@ router.post("/tmp_daseki_transmission", async (req, res, next) => {
 
 //試合情報送信（速報用）
 router.post("/tmp_daseki_call", async (req, res, next) => {
-    const { table_name , game_id} = req.body;
+    const { table_name, game_id } = req.body;
     const tmp_table_name = `test_pbl.` + table_name;
 
     try {
@@ -181,9 +181,9 @@ router.post("/tmp_daseki_call", async (req, res, next) => {
         const rows2 = await executeQuery(`select * from ${tmp_table_name} as daseki join (select player_id, player_name_kanji from t_player where school_id = ? or school_id = ?) as player using(player_id) order by at_bat_id`, [rows1[0]['school_id_1'], rows1[0]['school_id_2']]);
         //const rows = await executeQuery('select * from t_game as a join t_venue as venue using(venue_id) join (select school_id as school_id_1, school_name as school_name_1 from t_school) as b using(school_id_1) join (select school_id as school_id_2, school_name as school_name_2 from t_school) as c using(school_id_2)  join t_tournament as tournament using(tournament_id) where game_id = ?', [game_id]);
         // const rows = await executeQuery(`select * from ${tmp_table_name} as bat join (select * from t_starting_player where game_id = ?) as s_player using(player_id) join t_school as school on s_player.school_id = school.school_id order by at_bat_id desc limit 1`, [game_id]);
-         return res.json(rows2);
+        return res.json(rows2);
     }
-    catch (err) { 
+    catch (err) {
         console.log(err);
         //await tran.rollback();
         next(err);
@@ -199,13 +199,13 @@ router.post("/tmp_table_check", async (req, res, next) => {
     try {
         //試合情報の取得と送信(速報用)
         const rows = await executeQuery(`show tables like '${table_name}'`);
-        if(rows == 0 && rows == false){
+        if (rows == 0 && rows == false) {
             res.end("not exist");
-        }else{
+        } else {
             res.end("exist");
         }
     }
-    catch (err) { 
+    catch (err) {
         console.log(err);
         next(err);
     }
@@ -214,7 +214,7 @@ router.post("/tmp_table_check", async (req, res, next) => {
 //試合情報送信(過去データ参照用)
 router.post("/daseki_transmission", async (req, res, next) => {
     const { game_id } = req.body;
-    
+
 
     try {
         //試合情報の取得と送信
@@ -222,10 +222,10 @@ router.post("/daseki_transmission", async (req, res, next) => {
         const rows1 = await executeQuery('select * from t_game where game_id = ?', [game_id]);
         const rows2 = await executeQuery(`select * from t_at_bat as bat join (select player_id, player_name_kanji from t_player where school_id = ? or school_id = ?) as player using(player_id) where game_id = ?`, [rows1[0]['school_id_1'], rows1[0]['school_id_2'], game_id]);
         //const rows2 = await executeQuery(`select * from ${tmp_table_name} as daseki join (select player_id, player_name_kanji from t_player where school_id = ? or school_id = ?) as player using(player_id) order by at_bat_id`, [rows1[0]['school_id_1'], rows1[0]['school_id_2']]);
-        
+
         return res.json(rows2);
     }
-    catch (err) { 
+    catch (err) {
         console.log(err);
         next(err);
     }
@@ -235,18 +235,18 @@ router.post("/daseki_transmission", async (req, res, next) => {
 
 //試合中選手情報更新（運用者用webアプリ）
 router.post("/player_data_change", async (req, res, next) => {
-   
+
     const { player_id, game_id, position, batting_order } = req.body;
 
     const tran = await beginTran();
 
-    try{
+    try {
         //選手情報の編集
         await tran.query('update t_starting_player set position = ?, batting_order = ? where player_id = ? and game_id = ?', [position, batting_order, player_id, game_id]);
         await tran.commit();
         res.end("OK");
     }
-    catch(err){
+    catch (err) {
         console.log(err);
         await tran.rollback();
         next(err);
@@ -258,12 +258,12 @@ router.post("/tmp_daseki_update", async (req, res, next) => {
     const { table_name, at_bat_id, game_id, school_id, player_id, pitcher_id, score, total_score, batting_order, outcount, base, text_inf, pass, touched_coordinate, ball_kind, hit, foreball, deadball, pinch } = req.body;
     const tmp_table_name = `test_pbl.` + table_name;
 
-    try{
+    try {
         //試合情報の編集
         await executeQuery(`update ${tmp_table_name} set school_id = ?, player_id = ?, pitcher_id = ?, score = ?, total_score = ?, batting_order = ?, outcount = ?, base = ?, text_inf = ?, pass = ?, touched_coordinate = ?, ball_kind = ?, hit = ?, foreball = ?, deadball = ?, pinch = ? where at_bat_id = ? and game_id = ?`, [school_id, player_id, pitcher_id, score, total_score, batting_order, outcount, base, text_inf, pass, touched_coordinate, ball_kind, hit, foreball, deadball, pinch, at_bat_id, game_id]);
         res.end("OK");
     }
-    catch(err){
+    catch (err) {
         console.log(err);
         console.log('試合情報を登録できません');
         next(err);
@@ -276,12 +276,12 @@ router.post("/registered_daseki_update", async (req, res, next) => {
 
     //const tran = await beginTran();
 
-    try{
+    try {
         //試合情報の編集
         await executeQuery(`update t_at_bat set score = ?, total_score = ?, batting_order = ?, outcount = ?, base = ?, text_inf = ?, pass = ?, touched_coordinate = ?, ball_kind = ?, hit = ?, foreball = ?, deadball = ?, pinch = ? where at_bat_id = ? and game_id = ?`, [school_id, player_id, pitcher_id, score, total_score, batting_order, outcount, base, text_inf, pass, touched_coordinate, ball_kind, hit, foreball, deadball, pinch, at_bat_id, game_id]);
         res.end("OK");
     }
-    catch(err){
+    catch (err) {
         console.log('試合情報を登録できません');
         next(err);
     }
@@ -289,16 +289,68 @@ router.post("/registered_daseki_update", async (req, res, next) => {
 
 //速報中の試合の状況取得（運用者用webアプリ）
 router.post("/tmp_daseki_state", async (req, res, next) => {
-    const { table_name } = req.body;
-    const tmp_table_name = `test_pbl.` + table_name;
-
-    try{
+    const { tournament_id } = req.body;
+    //const tmp_table_name = `test_pbl.` + table_name;
+    try {
+        var gameStateArray = [];
+        const game_ids = await executeQuery('select game_id from t_game where tournament_id = ?  and school_id_1 is not null and school_id_2 is not null', [tournament_id]);
+        //速報中の試合テーブル一覧からgame_idが一致するテーブル名を出力
+        for (var game of game_ids) {
+            let gameIdArray = [{},{}]
+            let Inning = {};
+            let score = {'start':true}
+            let table_name = await executeQuery('select * from t_during_game where game_id = ?', [game['game_id']]);
+            let gameInfo = await executeQuery('select * from t_game where game_id = ?', [game['game_id']]); 
+            console.log(table_name)
+            if (table_name != 0 && table_name != false) {
+                let tmp_table_name = `test_pbl.` + table_name[0]['tmp_table_name'];
+                let school_id_1 = gameInfo[0]['school_id_1'];
+                let school_id_2 = gameInfo[0]['school_id_2'];
+                let rows = await executeQuery(`select inning from ${tmp_table_name} order by at_bat_id desc limit 1`);
+                let score1 = await executeQuery(`select total_score from ${tmp_table_name} where school_id = ? order by at_bat_id desc limit 1`, [school_id_1]);
+                let score2 = await executeQuery(`select total_score from ${tmp_table_name} where school_id = ? order by at_bat_id desc limit 1`, [school_id_2]);
+                let during = await executeQuery(`select count(*) from ${tmp_table_name}`);
+                if (rows == 0 && rows == false) {
+                    var inning = '１回表'
+                    Inning[game['game_id'].toString()] = inning;
+                } else {
+                    if (rows[0]['inning'] % 10 === 1) {
+                        var inning = Math.floor(rows[0]['inning'] / 10).toString() + '回表';
+                    } else {
+                        var inning = Math.floor(rows[0]['inning'] / 10).toString() + '回裏';
+                    }
+                    Inning[game['game_id'].toString()] = inning;
+                }
+                if(score1 == 0 && score1 == false){
+                    score['score1'] = 0;
+                }else{
+                    score['score1'] = score1[0]['total_score'];
+                }
+                
+                if(score2 == 0 && score2 == false){
+                    score['score2'] = 0
+                    score2 = 0;
+                }else{
+                    score['score2'] = score2[0]['total_score'];
+                } 
+                
+                if(during[0]['count(*)'] == 0){
+                    score['start'] = false;
+                }
+                gameIdArray[0] = Inning;
+                gameIdArray[1] = score;
+                gameStateArray.push(gameIdArray);
+            }
+        }
+        //const table_name = await executeQuery('select * from t_during_game where game_id = ?', [game_id]);
+        //console.log(table_name)
+        //const tmp_table_name = `test_pbl.` + table_name[0]['tmp_table_name'];
         //試合情報の編集
         //await executeQuery(`update ${tmp_table_name} set school_id = ?, player_id = ?, pitcher_id = ?, score = ?, total_score = ?, outcount = ?, base = ?, text_inf = ?, pass = ?, touched_coordinate = ?, ball_kind = ?, hit = ?, foreball = ?, deadball = ?, pinch = ? where at_bat_id = ? and game_id = ?`, [school_id, player_id, pitcher_id, score, total_score, outcount, base, text_inf, pass, touched_coordinate, ball_kind, hit, foreball, deadball, pinch, at_bat_id, game_id]);
-        const rows = await executeQuery(`select inning from ${tmp_table_name} order by inning desc limit 1`);
-        return res.json(rows);
+        //console.log(inning)
+        return res.json(gameStateArray);
     }
-    catch(err){
+    catch (err) {
         console.log(err);
         console.log('試合情報を登録できません');
         next(err);

ca7a7dc51cc4595f365fd4cf5937420940e1b489
e1766mori <j846029b@mails.cc.ehime-u.ac.jp>
2023-02-02 13:30:28 +0900

    0202

--- front-output/src/DB/communication.js
+++ front-output/src/DB/communication.js
@@ -1,6 +1,6 @@
-// const backendUrl = "http://133.71.101.108:5000"
-export const backendUrl = "http://localhost:5000"
+export const backendUrl = "http://133.71.101.108:5000"
+// export const backendUrl = "http://localhost:5000"
 
-// const routeUrl = "/j_R4_team_b/frontend"
-// const routeUrl = "/team-b-project/frontend"
-export const routeUrl = ""
+// const routeUrl = "/j_R4_team_b/front-output"
+export const routeUrl = "/team-b-project/front-output"
+// export const routeUrl = ""
